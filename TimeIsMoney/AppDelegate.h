ed212af7cc4a8f7d4e613a923107d24f53c96853
Brian Lane <brian@gourmetgaming.com>
2016-08-05 13:59:39 -0400

    Initial Commit

---
+++ TimeIsMoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeIsMoney
+//
+//  Created by Brian Lane on 8/5/16.
+//  Copyright © 2016 Brian Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeIsMoney/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeIsMoney
+//
+//  Created by Brian Lane on 8/5/16.
+//  Copyright © 2016 Brian Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
